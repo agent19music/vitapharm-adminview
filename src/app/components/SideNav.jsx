6869b87de35a7b64cb79b921962b102d407d6444
agent19music <seanmotanya@gmail.com>
2024-07-23 14:52:27 +0300

    Add curren route acive nav icon syling

--- src/app/components/SideNav.jsx
+++ src/app/components/SideNav.jsx
@@ -1,115 +1,114 @@
+import { usePathname } from 'next/navigation';
 import {
-    TooltipProvider,  
-    Tooltip,
-    TooltipContent,
-    TooltipTrigger,
-  } from "@/components/ui/tooltip"
-  import Link from "next/link"
-import { Package2, Home, ShoppingCart, Package, Users2, LineChart , Settings} from "lucide-react"
+  TooltipProvider,
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import Link from 'next/link';
+import { Package2, Home, ShoppingCart, Package, Users2, LineChart, Settings, Percent } from 'lucide-react';
 
 export default function SideNav() {
+  
+  const currentPath = usePathname();
+
+  const getLinkClasses = (path) =>
+    `flex h-9 w-9 items-center justify-center rounded-lg ${
+      currentPath === path
+        ? 'bg-accent text-accent-foreground'
+        : 'text-muted-foreground hover:text-foreground'
+    } transition-colors md:h-8 md:w-8`;
+
   return (
     <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
-       
-    <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
-      <Link
-        href="#"
-        className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
-      >
-        <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
-        <span className="sr-only">Acme Inc</span>
-      </Link>
-      <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Link
-            href="/dashboard"
-            className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-          >
-            <Home className="h-5 w-5" />
-            <span className="sr-only">Dashboard</span>
-          </Link>
-        </TooltipTrigger>
-        <TooltipContent side="right">Dashboard</TooltipContent>
-      </Tooltip>
-      </TooltipProvider>
-      <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Link
-            href="/orders"
-            className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-          >
-            <ShoppingCart className="h-5 w-5" />
-            <span className="sr-only">Orders</span>
-          </Link>
-        </TooltipTrigger>
-        <TooltipContent side="right">Orders</TooltipContent>
-      </Tooltip>
-      </TooltipProvider>
-      <TooltipProvider>
-      <Tooltip>
-        
-        <TooltipTrigger asChild>
-          <Link
-            href="/products"
-            className="flex h-9 w-9 items-center justify-center rounded-lg  bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-          >
-            <Package className="h-5 w-5" />
-            <span className="sr-only">Products</span>
-          </Link>
-        </TooltipTrigger>
-        <TooltipContent side="right">Products</TooltipContent>
-      </Tooltip>
-      </TooltipProvider>
-      <TooltipProvider>
-      <Tooltip>
-        
-        <TooltipTrigger asChild>
-          <Link
-            href="#"
-            className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-          >
-            <Users2 className="h-5 w-5" />
-            <span className="sr-only">Customers</span>
-          </Link>
-        </TooltipTrigger>
-        <TooltipContent side="right">Customers</TooltipContent>
-      </Tooltip>
-      </TooltipProvider>
-      <TooltipProvider>
-      <Tooltip>
-        
-        <TooltipTrigger asChild>
-          <Link
-            href="#"
-            className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-          >
-            <LineChart className="h-5 w-5" />
-            <span className="sr-only">Analytics</span>
-          </Link>
-        </TooltipTrigger>
-        <TooltipContent side="right">Analytics</TooltipContent>
-      </Tooltip>
-      </TooltipProvider>
-    </nav>
-    <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
+      <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
+        <Link
+          href="#"
+          className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+        >
+          <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
+          <span className="sr-only">Acme Inc</span>
+        </Link>
         <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Link
-            href="#"
-            className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-          >
-            <Settings className="h-5 w-5" />
-            <span className="sr-only">Settings</span>
-          </Link>
-        </TooltipTrigger>
-        <TooltipContent side="right">Settings</TooltipContent>
-      </Tooltip>
-      </TooltipProvider>
-    </nav>
-    {/* SIDE NAV ENDS HERE */}
-  </aside>
-  )
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link href="/dashboard" className={getLinkClasses('/dashboard')}>
+                <Home className="h-5 w-5" />
+                <span className="sr-only">Dashboard</span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent side="right">Dashboard</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link href="/orders" className={getLinkClasses('/orders')}>
+                <ShoppingCart className="h-5 w-5" />
+                <span className="sr-only">Orders</span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent side="right">Orders</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link href="/products" className={getLinkClasses('/products')}>
+                <Package className="h-5 w-5" />
+                <span className="sr-only">Products</span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent side="right">Products</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link href="/customers" className={getLinkClasses('/customers')}>
+                <Users2 className="h-5 w-5" />
+                <span className="sr-only">Customers</span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent side="right">Customers</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link href="/promocode" className={getLinkClasses('/promocode')}>
+                <Percent className="h-5 w-5" />
+                <span className="sr-only">Promocode</span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent side="right">Promocode</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link href="/analytics" className={getLinkClasses('/analytics')}>
+                <LineChart className="h-5 w-5" />
+                <span className="sr-only">Analytics</span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent side="right">Analytics</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </nav>
+      <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link href="/settings" className={getLinkClasses('/settings')}>
+                <Settings className="h-5 w-5" />
+                <span className="sr-only">Settings</span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent side="right">Settings</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </nav>
+    </aside>
+  );
 }
